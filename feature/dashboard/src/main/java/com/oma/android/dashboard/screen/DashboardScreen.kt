c69e04d9ea93b76d60a4ac0743df4a5adb54ada6
MF11471 Dharshan J <dharshan.j@modefin.com>
2025-04-26 12:47:51 +0530

    pan when keyboard is open status bar color using sideaffect

--- feature/dashboard/src/main/java/com/oma/android/dashboard/screen/DashboardScreen.kt
+++ feature/dashboard/src/main/java/com/oma/android/dashboard/screen/DashboardScreen.kt
@@ -1,10 +1,17 @@
 package com.oma.android.dashboard.screen
 
+import android.app.Activity
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
+import com.oma.android.composeui.theme.ColorPacket
+import com.oma.android.composeui.theme.Primary
 import com.oma.android.composeui.theme.Themer
 import com.oma.android.dashboard.BottomNavBar
 import com.oma.android.dashboard.DashboardSharedViewModel
@@ -13,6 +20,16 @@ import com.oma.android.dashboard.navhost.DashboardNavHost
 @Composable
 fun DashboardScreen(viewModel: DashboardSharedViewModel) {
     val navController = rememberNavController()
+    val activity = LocalContext.current as Activity
+
+    // Set status bar color using Android API
+    SideEffect {
+        val window = activity.window
+        window.statusBarColor = Primary.toArgb()
+
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+        insetsController.isAppearanceLightStatusBars = false
+    }
     Scaffold(
         containerColor = Themer.colors.Mercury,
         bottomBar = {
